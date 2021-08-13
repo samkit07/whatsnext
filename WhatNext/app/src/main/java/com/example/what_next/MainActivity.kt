8cba5b07ff5c852b2978694df596cc8751b3fb6f
mustafaDalal <87816730+mustafaDalal@users.noreply.github.com>
2021-08-13 14:38:47 +0530

    Project

---
+++ WhatNext/app/src/main/java/com/example/what_next/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.what_next
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
