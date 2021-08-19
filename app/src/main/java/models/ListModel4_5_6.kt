ae1c1d51656352a443f5576801e033c3ac51740e
mustafaDalal <mustafadalal.md@gmail.com>
2021-08-19 12:04:54 +0530

    added ListModel for remaining pages

---
+++ app/src/main/java/models/ListModel4_5_6.kt
@@ -0,0 +1,21 @@
+package models
+
+import com.google.gson.annotations.SerializedName
+
+class ListModel4 {
+
+    @SerializedName("srNo")
+    val srNo:Int=0
+    @SerializedName("name")
+    val name:String=""
+    @SerializedName("conductedBy")
+    val conductedBy:String=""
+    @SerializedName("applicationFor")
+    val applicationFor:String=""
+    @SerializedName("notificationMonth")
+    val notificationMonth:String=""
+    @SerializedName("modeOfSelection")
+    val modeOfSelection:String=""
+    @SerializedName("website")
+    val website:String=""
+}
