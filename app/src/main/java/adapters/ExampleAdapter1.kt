9b7b63ab01fbdf7c54beca11823359e7d79064a3
mustafaDalal <mustafadalal.md@gmail.com>
2021-08-23 20:54:19 +0530

    Partial display for nested objects

--- app/src/main/java/adapters/ExampleAdapter1.kt
+++ app/src/main/java/adapters/ExampleAdapter1.kt
@@ -1,17 +1,28 @@
 package adapters
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent.getActivity
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.RecycledViewPool
+import com.example.whatnext.MainActivity
 import com.example.whatnext.R
 import models.*
 
 class ExampleAdapter1(
     private val exampleList: ArrayList<Model1>
 ) : RecyclerView.Adapter<ExampleAdapter1.ExampleViewHolder>() {
+
+    // the parent RecyclerViews
+    private val viewPool = RecycledViewPool()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
 
@@ -23,29 +34,25 @@ class ExampleAdapter1(
         return ExampleViewHolder(itemView)
     }
 
+    @SuppressLint("WrongConstant")
     override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
 
+
         val currentCourse = exampleList[position]
-
-//        holder.textView1.text = currentCourse.name
-
-//        for(i in 0 until currentCourse.type.size){
 
         holder.textView1.text = currentCourse.type
         holder.textView2.text = currentCourse.name
         if (currentCourse.duration != "") holder.textView3.text = currentCourse.duration
 
-//        holder.textView3.text = currentCourse.fields.toString()
-//        }
+        holder.recycler_view1.adapter = ExampleAdapter1_1(currentCourse.fields)
+        holder.recycler_view1.layoutManager =LinearLayoutManager(holder.recycler_view1.context,
+            LinearLayout.HORIZONTAL, false)
+
+        holder.recycler_view1.setHasFixedSize(true)
 
 //        Log.i("position", "$position")
-        Log.i("itemCount", "$itemCount")
-//        holder.textView2.text = currentCourse.type.toString().replace(Regex("(^\\[|\\]$)"), "")
-//            .replace(",","\n")
-//        holder.textView3.text = currentCourse.type.
-//            holder.textView2.text = currentCourse.type.name
-//        holder.textView3.text = currentCourse.type[0].fields.toString()
-//            holder.textView3.text = currentCourse.type[position].duration
+//        Log.i("itemCount", "$itemCount")
+
     }
 
     override fun getItemCount() = exampleList.size
@@ -55,6 +62,9 @@ class ExampleAdapter1(
         val textView1: TextView = itemView.findViewById(R.id.textview1_1)
         val textView2: TextView = itemView.findViewById(R.id.textview2_1)
         val textView3: TextView = itemView.findViewById(R.id.textview3_1)
+        val recycler_view1 = itemView.findViewById<RecyclerView>(R.id.recyclerview1)
+
+
     }
 
 
--- app/src/main/java/adapters/ExampleAdapter1_1.kt
+++ app/src/main/java/adapters/ExampleAdapter1_1.kt
@@ -10,15 +10,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.whatnext.R
 import models.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.whatnext.MainActivity
+
 
 class ExampleAdapter1_1(
-    private val exampleList: ArrayList<Model1>
+    private val exampleList: ArrayList<Fields>
 ) : RecyclerView.Adapter<ExampleAdapter1_1.ExampleViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
 
         val itemView = LayoutInflater.from(parent.context).inflate(
-            R.layout.example_course1,
+            R.layout.example_course1_1,
             parent, false
         )
 
@@ -29,9 +32,9 @@ class ExampleAdapter1_1(
 
         val currentCourse = exampleList[position]
 
-        holder.textView1.text = currentCourse.type
-        holder.textView2.text = currentCourse.name
-        if (currentCourse.duration != "") holder.textView3.text = currentCourse.duration
+        holder.textView1_1_1.text = currentCourse.name
+        holder.textView2_1_1.text = currentCourse.subFields.toString()
+//        if (currentCourse.duration != "") holder.textView3.text = currentCourse.duration
 
         Log.i("position", "$position")
         Log.i("itemCount", "$itemCount")
@@ -42,9 +45,10 @@ class ExampleAdapter1_1(
 
     class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        val textView1: TextView = itemView.findViewById(R.id.textview1_1)
-        val textView2: TextView = itemView.findViewById(R.id.textview2_1)
-        val textView3: TextView = itemView.findViewById(R.id.textview3_1)
+        val textView1_1_1: TextView = itemView.findViewById(R.id.textview1_1_1)
+        val textView2_1_1: TextView = itemView.findViewById(R.id.textview2_1_1)
+
+
     }
 
 
